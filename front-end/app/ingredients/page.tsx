e3f03e78a4f91a59c9dfa859e8c68f0a8c59a3be
Bradley-Hill <brad.michael.hill.com>
2024-01-11 11:35:16 +0100

    testing multi page routing

---
+++ front-end/app/ingredients/page.tsx
@@ -0,0 +1,8 @@
+export default function IngredientsPage() {
+  return (
+    <div>
+      <h1>Ingredients Page</h1>
+      <p> Lorem Ipsum is my Ingredient.</p>
+    </div>
+  );
+}
---
+++ front-end/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login page</h1>
+      <p> here will be my Login.</p>
+    </div>
+  );
+}
