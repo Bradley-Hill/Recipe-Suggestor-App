ec2ad1700303a2e1d80737b7e9e4991c4c7d1598
Bradley-Hill <brad.michael.hill.com>
2024-04-18 18:13:46 +0200

    re-installed Vue in place of next

--- front-end/app/addRecipe/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AddRecipePage() {
-  return (
-    <div>
-      <h1>Adding a Recipe Page</h1>
-      <p> Lorem Ipsum is my new recipe.</p>
-    </div>
-  );
-}
--- front-end/app/login/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function LoginPage() {
-  return (
-    <div>
-      <h1>Login page</h1>
-      <p> here will be my Login.</p>
-    </div>
-  );
-}
