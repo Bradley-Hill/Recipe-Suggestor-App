35873d8721a0513d3925f44b1079800b403735bc
Bradley-Hill <brad.michael.hill.com>
2024-04-18 20:24:06 +0200

    Refactor front-end code by removing unused files

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+    <h1>Welcome to Home Page</h1>
+    <!-- Your code goes here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+  // Your component logic goes here
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+/* Your styles go here */
+</style>
