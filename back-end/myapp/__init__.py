d289e2f18b1e1d94aba1e79d0eabdc25d8657b49
Bradley-Hill <brad.michael.hill.com>
2024-05-02 15:21:42 +0200

    added CORS and imported endpoints from Views to run.py

--- back-end/myapp/__init__.py
+++ back-end/myapp/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+CORS(app)
 app.config["MONGO_URI"] = (
     "mongodb+srv://bradleyhill:YDBJrnsC7CG8WzXg@testrecipesuggestions.mtjmyrr.mongodb.net/?retryWrites=true&w=majority&appName=testRecipeSuggestions"
 )
--- back-end/run.py
+++ back-end/run.py
@@ -1,4 +1,5 @@
 from myapp import app
+import myapp.views
 
 if __name__ == "__main__":
     app.run(debug=True)
