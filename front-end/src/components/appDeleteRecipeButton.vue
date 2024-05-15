b0610b0eff846cbf614414321033a655b9630e95
Bradley-Hill <brad.michael.hill.com>
2024-05-15 16:31:53 +0200

    created component for teh delete recipe button

---
+++ front-end/src/components/appDeleteRecipeButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button type="button" class="deleteBtn" v-on:click="deleteRecipe">Delete</button>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'DeleteRecipeButton',
+  props: {
+    recipeId: String
+  },
+  methods: {
+    deleteRecipe() {
+      axios({
+        method: 'delete',
+        url: 'http://localhost:5000/delete_recipe',
+        data: { _id: this.recipeId }
+      })
+        .then((response) => {
+          console.log(response.data)
+          this.$emit('recipeDeleted', this.recipeId)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped></style>
