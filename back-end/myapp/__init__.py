d154d6edd2a590560c6e3eb0e28ad473fb94e1cb
Bradley-Hill <brad.michael.hill.com>
2024-05-17 10:37:19 +0200

    added TEXT index to ingredients field in Recipes collection.

--- back-end/myapp/__init__.py
+++ back-end/myapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from pymongo import MongoClient
+from pymongo import TEXT
 from flask_cors import CORS
 
 print("MongDB client init...")
@@ -10,6 +11,12 @@ mongo_connection_str = "mongodb+srv://bradleyhill:YDBJrnsC7CG8WzXg@testrecipesug
 client = MongoClient(mongo_connection_str)
 
 db = client["testRecipeSuggestions"]
+
+# ensure that the Recipes collection has text index assigned to "ingredients" field for search
+indexes = db["Recipes"].list_indexes()
+if not any(index["name"] == "ingredients_text" for index in indexes):
+    db["Recipes"].create_index([("ingredients", TEXT)])
+
 app.config["db"] = db
 
 import myapp.views
