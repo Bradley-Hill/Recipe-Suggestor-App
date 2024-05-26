bf3dc364cd289b5c113175c4c34924ff8af90002
Bradley-Hill <brad.michael.hill.com>
2024-05-26 13:37:49 +0200

    added Recipe interface to use, and finished the populating of results using recipeDisplayCard component

---
+++ front-end/src/interfaces/Recipe.ts
@@ -0,0 +1,8 @@
+export interface Recipe {
+  _id: string
+  name: string
+  total_time: number
+  image_url: string
+  ingredients: string[]
+  instructions: string[]
+}
--- front-end/src/views/ResultsView.vue
+++ front-end/src/views/ResultsView.vue
@@ -3,23 +3,52 @@
     <h1>Results Page</h1>
   </div>
   <div class="searchForm">
-    <AppIngredientForm></AppIngredientForm>
+    <AppIngredientForm @search-results="handleSearchResults"></AppIngredientForm>
   </div>
   <div class="recipeDisplayContainer">
-    <appRecipeDisplayCard></appRecipeDisplayCard>
+    <p v-if="noResults">No recipes found</p>
+    <appRecipeDisplayCard
+      v-else
+      v-for="recipe in recipes"
+      :key="recipe._id"
+      :recipe="recipe"
+    ></appRecipeDisplayCard>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
 import AppIngredientForm from '@/components/appIngredientForm.vue'
 import appRecipeDisplayCard from '@/components/appRecipeDisplayCard.vue'
-export default {
+import type { Recipe } from '@/interfaces/Recipe'
+export default defineComponent({
   name: 'ResultsView',
   components: { AppIngredientForm, appRecipeDisplayCard },
-  data() {
-    return { recipes: [] }
+  data(): { recipes: Recipe[]; noResults: boolean } {
+    return { recipes: [], noResults: false }
+  },
+  computed: {
+    hasResults() {
+      return computed(() => this.recipes.length > 0)
+    }
+  },
+  methods: {
+    handleSearchResults(results: Recipe[]) {
+      if (results.length === 0) {
+        this.noResults = true
+      } else {
+        this.noResults = false
+        this.recipes = results
+      }
+    }
   }
-}
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+.recipeDisplayContainer {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(19rem, 1fr));
+  grid-gap: 1rem;
+}
+</style>
