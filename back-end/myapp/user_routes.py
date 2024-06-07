8c1b00e7424f8969984f23ace9e50ea26332764b
Bradley Hill <brad.michael.hill@gmail.com>
2024-06-07 15:29:26 +0200

    added data validation to create_user route

--- back-end/myapp/user_routes.py
+++ back-end/myapp/user_routes.py
@@ -1,3 +1,25 @@
 # User and login/auth routes to be kept here
+import re
 from flask import current_app, request, jsonify, make_response
 from myapp import app
+
+@app.route("/createUser", methods=["POST"])
+def create_user():
+    try:
+        db = current_app.config["db"]
+        username = request.json.get("username")
+        email = request.json.get("email")
+        password = request.json.get("password")
+        confirm_password = request.json.get("confirmPassword")
+        # Data Validation
+        if password != confirm_password:
+            return make_response(jsonify(error="Passwords do not match"), 400)
+        if db.Users.find({"email": email}).count() > 0:
+            return make_response(jsonify(error="Email already exists"), 400)
+        if username == "" or email == "" or password == "":
+            return make_response(jsonify(error="All fields are required"), 400)
+        if len(password) < 8:
+            return make_response(jsonify(error="Password must be at least 8 characters"), 400)
+        match = re.match(r"[^@]+@[^@]+\.[^@]+", email)
+        if match is None:
+            return make_response(jsonify(error="Invalid email"), 400)
