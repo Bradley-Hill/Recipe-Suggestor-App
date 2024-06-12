11733b827dd4a4867a8eb8775a2e17509b578a26
Bradley Hill <brad.michael.hill@gmail.com>
2024-06-12 14:42:25 +0200

    added axiosConfig to create an axios interceptor that adds teh JWT to Authorization  header of each request.

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/global.css'
+import "../src/config/axiosConfig"
 
 const app = createApp(App)
 
